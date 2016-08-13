faacb1fa49af322672f3b29f3a63e8736fccddab
paulo <paulo.szpikula10@hotmail.com>
2016-08-13 18:50:41 -0300

    #4 Criados dois EJBs Stateles ProcessVenda e ProcessEntrega.

---
+++ lab141115/src/main/java/ejbs/ProcessEntrega.java
@@ -0,0 +1,13 @@
+package ejbs;
+
+import javax.ejb.Stateless;
+
+import modelos.Entrega;
+
+@Stateless
+public class ProcessEntrega {
+
+	public void processarEntrega (Entrega entrega) {
+
+    }
+}
---
+++ lab141115/src/main/java/ejbs/ProcessVenda.java
@@ -0,0 +1,13 @@
+package ejbs;
+
+import javax.ejb.Stateless;
+
+import modelos.Venda;
+
+@Stateless
+public class ProcessVenda {
+
+	public void processarVenda (Venda venda) {
+
+    }
+}
